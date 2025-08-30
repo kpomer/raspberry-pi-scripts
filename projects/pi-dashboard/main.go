b04a69f40ff7bde693fdc30bb4f12faa44672f56
kpomer <pomerkevin@gmail.com>
2025-08-30 23:22:58 +0300

    basic text printout with sample values

--- projects/pi-dashboard/main.go
+++ projects/pi-dashboard/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//"os/exec"
+	//"runtime"
+	//"github.com/gizak/termui/v3"
+	//"github.com/gizak/termui/v3/widgets"
+)
 
+// ---- Metrics Functions ----
+func getCPUTemp() string {
+	// TODO Retrieve Actual Data
+	return "50C"
+}
+
+func getDiskUsage() string {
+	// TODO Retrieve Actual Data
+	return "10MB"
+}
+
+func getMemoryUsage() string {
+	// TODO Retrieve Actual Data
+	return "80GB"
+}
+
+// ---- Main Program ----
 func main() {
-    fmt.Println("Hello, world!")
+    // 1. Initialize termui
+    // 2. Create widgets
+    // 3. Render widgets
+    // 4. Event loop
+
+	cpuTemp := getCPUTemp()
+	diskUsage := getDiskUsage()
+	memoryUsage := getMemoryUsage()
+	
+	fmt.Printf("Current CPU Temp: %s\nCurrent Disk Usage: %s\nCurrent Memory Usage: %s\n", cpuTemp, diskUsage, memoryUsage)
+
 }
