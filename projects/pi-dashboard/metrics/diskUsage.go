427210bfa64f394cba9be15ada895c055b122066
kpomer <pomerkevin@gmail.com>
2025-09-01 17:14:17 +0300

    added more to disk usage

--- projects/pi-dashboard/metrics/diskUsage.go
+++ projects/pi-dashboard/metrics/diskUsage.go
@@ -13,14 +13,12 @@ func GetDiskUsage(sampleData bool) [][]string {
 		// return sample data
 		val_Str = getSampleDiskUsage()
 	} else {
-		// cmd: df -h /
-		cmdOutput, err := exec.Command("df", "-h", "/").Output()
+		// cmd: df -h / /mnt/timemachine /boot/firmware 2>/dev/null
+		cmdOutput, err := exec.Command("sh", "-c", "df -h / /mnt/timemachine /boot/firmware 2>/dev/null").Output()
+		val_Str = strings.TrimSpace(string(cmdOutput)) // convert []byte to string (ignore errors)
         if err != nil {
             fmt.Println("Error running command:", err)
-			val_Str = ""
-        } else {
-			val_Str = strings.TrimSpace(string(cmdOutput)) // convert []byte to string
-		}
+        }
 	}
 
 		val_Str = strings.Replace(val_Str, "Mounted on", "Mounted_On", -1)
@@ -49,7 +47,6 @@ func getSampleDiskUsage() string {
 	// SAMPLE RESPONSE:
 	// Filesystem      Size  Used Avail Use% Mounted on
 	// /dev/mmcblk0p2   59G  2.6G   53G   5% /
-	
 	
 	duStr := "Filesystem      Size  Used Avail Use% Mounted on\n/dev/mmcblk0p2   59G  2.6G   53G   5% /\n/dev/mmcblk0p8   75G  32.5G   90G   30% /mnt"
 	return duStr
